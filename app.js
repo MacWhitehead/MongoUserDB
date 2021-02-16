b2562753436a9bf63d6e0d3fd0d17a705d41eed8
MacWhitehead <mackayla.whitehead7485@stu.mtec.edu>
2021-02-16 16:32:53 -0700

    Added search last & first, working sort asc/desc

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const dbConnectionString = "mongodb://localhost/mtech";
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +17,7 @@ mongoose.connect(dbConnectionString, {
   useUnifiedTopology: true,
 });
 const udb = mongoose.connection;
+mongoose.set('useFindAndModify', false)
 
 const userSchema = new mongoose.Schema({
   userID: String,
@@ -32,11 +33,11 @@ app.get("/", (req, res) => {
   res.render("index", {});
 });
 
-app.get('/userData', (req, res) => {
+app.get("/userData", (req, res) => {
   users.find({}, (err, data) => {
-    res.render('viewUsers', { users: data})
-  })
-})
+    res.render("viewUsers", { users: data });
+  });
+});
 
 app.post("/newUserSubmitted", (req, res) => {
   const newUser = new users();
@@ -53,19 +54,64 @@ app.post("/newUserSubmitted", (req, res) => {
   res.redirect("/userData");
 });
 
-app.get(('editUser/:userID', (req, res) => {
-  users.findOne({userID: req.params.userID}, (err, data) => {
-    res.render('editUser', {user: data})
-  })
-}))
+app.get("/editUser/:userID", (req, res) => {
+  users.findOne({ userID: req.params.userID }, (err, data) => {
+    res.render("editUser", { user: data });
+  });
+});
+
+app.post("/userEdited/:userID", (req, res) => {
+  let editedUser = req.body.userID;
+  users.findOneAndUpdate(
+    { usersID: editedUser },
+    {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      age: req.body.age,
+    },
+    (err, data) => {
+      if (err) throw err;
+      res.redirect("/userData");
+    }
+  );
+});
 
 app.post("/delete/:index", (req, res) => {
   let matchedName = req.body.firstName;
   users.findOneAndDelete({ name: matchedName }, (err, data) => {
     if (err) return console.log(`Oops! ${err}`);
-    res.redirect('/userData');
+    res.redirect("/userData");
   });
 });
+
+app.post('/searchByFirst/', (req, res) => {
+  let firstNameSearch = req.body.searchByFirst;
+  users.find({}, (err, data) =>{
+    if (err) throw err; 
+    let filteredUsers = data.filter(users => {
+      let searchingByFirst = users.first_name;
+      return searchingByFirst === firstNameSearch
+    })
+    res.render('viewUsers', {users: filteredUsers})
+  })
+})
+
+app.post('/searchByLast/', (req, res) => {
+  let lastNameSearch = req.body.searchByLast;
+  users.find({}, (err, data) =>{
+    if (err) throw err; 
+    let filteredUsers = data.filter(users => {
+      let searchingByLast = users.last_name;
+      return lastNameSearch === searchingByLast
+    })
+    res.render('viewUsers', {users: filteredUsers})
+  })
+})
+
+app.post('/sortAsc', (req, res) => {
+  users.find({}).sort('asc')
+})
 
 app.listen(port, (err) => {
   if (err) console.log(err);
